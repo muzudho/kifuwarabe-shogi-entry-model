ba5b6f9f16f9ad6db55f25e80347509484839b11
むずでょ <muzudho1@gmail.com>
2020-08-10 21:04:22 +0900

    モジュール整理中☆（＾～＾）

--- src/cosmic/recording.rs
+++ src/cosmic/recording.rs
@@ -7,7 +7,7 @@
 use crate::law::cryptographic::num_to_lower_case;
 use crate::look_and_model::{
     recording::{Movement, Phase},
-    AbsoluteAddress2D, DoubleFacedPiece, PieceNum,
+    AbsoluteAddress2D, DoubleFacedPiece,
 };
 use std::fmt;
 
@@ -152,68 +152,5 @@ impl CapturedMove {
             source: source,
             destination: destination,
         }
-    }
-}
-
-impl Default for Movement {
-    /// ゴミの値を作るぜ☆（＾～＾）
-    fn default() -> Self {
-        Movement {
-            piece_num: PieceNum::King1,
-            source: FireAddress::default(),
-            destination: FireAddress::default(),
-            promote: false,
-            captured: None,
-        }
-    }
-}
-impl Movement {
-    pub fn new(
-        piece_num: PieceNum,
-        source: FireAddress,
-        destination: FireAddress,
-        promote: bool,
-        captured: Option<CapturedMove>,
-    ) -> Self {
-        Movement {
-            piece_num: piece_num,
-            source: source,
-            destination: destination,
-            promote: promote,
-            captured: captured,
-        }
-    }
-}
-impl fmt::Display for Movement {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.source {
-            FireAddress::Board(src_sq) => {
-                let (sx, sy) = src_sq.to_file_rank();
-                write!(
-                    f,
-                    "{}{}{}{}",
-                    sx,
-                    num_to_lower_case(sy as usize),
-                    self.destination,
-                    if self.promote { "+" } else { "" }
-                )
-            }
-            FireAddress::Hand(_src_drop) => write!(
-                f,
-                "{}{}{}",
-                self.source, // src_drop,
-                self.destination,
-                if self.promote { "+" } else { "" }
-            ),
-        }
-    }
-}
-impl fmt::Debug for Movement {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Movement({:?}{:?}{})",
-            self.source, self.destination, self.promote,
-        )
     }
 }
--- src/look_and_model/recording/movement.rs
+++ src/look_and_model/recording/movement.rs
@@ -1,1 +1,69 @@
+use crate::{
+    cosmic::recording::{CapturedMove, FireAddress},
+    law::cryptographic::num_to_lower_case,
+    look_and_model::{recording::Movement, PieceNum},
+};
+use std::fmt;
 
+impl Default for Movement {
+    /// ゴミの値を作るぜ☆（＾～＾）
+    fn default() -> Self {
+        Movement {
+            piece_num: PieceNum::King1,
+            source: FireAddress::default(),
+            destination: FireAddress::default(),
+            promote: false,
+            captured: None,
+        }
+    }
+}
+impl Movement {
+    pub fn new(
+        piece_num: PieceNum,
+        source: FireAddress,
+        destination: FireAddress,
+        promote: bool,
+        captured: Option<CapturedMove>,
+    ) -> Self {
+        Movement {
+            piece_num: piece_num,
+            source: source,
+            destination: destination,
+            promote: promote,
+            captured: captured,
+        }
+    }
+}
+impl fmt::Display for Movement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.source {
+            FireAddress::Board(src_sq) => {
+                let (sx, sy) = src_sq.to_file_rank();
+                write!(
+                    f,
+                    "{}{}{}{}",
+                    sx,
+                    num_to_lower_case(sy as usize),
+                    self.destination,
+                    if self.promote { "+" } else { "" }
+                )
+            }
+            FireAddress::Hand(_src_drop) => write!(
+                f,
+                "{}{}{}",
+                self.source, // src_drop,
+                self.destination,
+                if self.promote { "+" } else { "" }
+            ),
+        }
+    }
+}
+impl fmt::Debug for Movement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Movement({:?}{:?}{})",
+            self.source, self.destination, self.promote,
+        )
+    }
+}
